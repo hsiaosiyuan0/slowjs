bf2eeb59ca75bcc39ae9fe510019502c1e94a3dc
hsiaosiyuan0 <hsiaosiyuan0@gmail.com>
2023-02-08 01:15:57 +0800

    build: move test262.patch

--- tests/run-test262/setup_test262.sh
+++ tests/run-test262/setup_test262.sh
@@ -8,6 +8,6 @@ data_dir="${script_dir}/test262"
 if [ ! -d "${data_dir}" ]; then
 	git clone https://github.com/tc39/test262.git "${data_dir}"
 	cd "${data_dir}"
-	patch -p1 < ../../test262.patch
+	patch -p1 < ../test262.patch
 	cd ..
 fi
