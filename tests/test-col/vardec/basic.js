5d533664f9d59b3917658d2e57d45916ec910be9
hsiaosiyuan0 <hsiaosiyuan0@gmail.com>
2023-02-15 11:11:38 +0800

    feat: test col for vardec/basic

--- tests/test-col/vardec/basic.js
+++ tests/test-col/vardec/basic.js
@@ -1,1 +1,26 @@
-print("test");
+var a = 1;
+let b = 1,
+	c;
+const d = 1;
+
+/* EXPECT(a):
+ident: 'a' 1:5
+token: '=' 1:7
+number: 1 1:9
+*/
+
+/* EXPECT(b):
+ident: 'b' 2:5
+token: '=' 2:7
+number: 1 2:9
+*/
+
+/* EXPECT(c):
+ident: 'c' 3:2
+*/
+
+/* EXPECT(d):
+ident: 'd' 4:7
+token: '=' 4:9
+number: 1 4:11
+*/
